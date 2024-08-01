2d8820b8cf2998ba5036a7f10ae30b86361241e8
lgio-Priyadeep <priyadeepmitra0321@gmail.com>
2024-08-01 14:54:39 +0530

    File1 was added for records

---
+++ File1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class File1(){
+    public static void main(String args[]){
+        System.out.println("NewFileToEnrterLog");
+        
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 # Starting out
 Starting out to learn and explore github usages 
 
+added File1.java
